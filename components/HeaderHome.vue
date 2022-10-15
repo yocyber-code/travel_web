57e3570d3db03f6c88a0179e9d5598c181d342d5
yoshi <yoshi@yoshis-MacBook-Pro.local>
2022-10-16 02:49:26 +0700

    pre index

---
+++ components/HeaderHome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header-container" :class="theme">
+    <button @click="onOpenSidebar">aaa</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderHome',
+  data() {
+    return {
+      theme: '',
+    }
+  },
+  created() {
+    this.theme = this.$store.getters['getterTheme']
+  },
+  methods: {
+    onOpenSidebar() {
+      this.$emit('onClickSidebar')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  height: 80px;
+  width: 100%;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+  &.lightblue {
+    background-color: #6ccdf3;
+  }
+}
+</style>
---
+++ components/SidebarNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sidebar-container" :class="active"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: String,
+      default: 'inactive',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scope>
+.sidebar-container {
+  transition: all 2s;
+  height: 100vh;
+  width: 200px;
+  position: fixed;
+  top: 80px;
+  left: 0px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
+
+.active {
+  display: block;
+}
+
+.inactive {
+  display: none;
+}
+</style>
